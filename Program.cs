a807abc49c035b923d425f459f14bce35cc2362f
Deon Pieterse <deon.pieterse@entelect.co.za>
2023-07-27 19:04:18 +0200

    S2

--- Program.cs
+++ Program.cs
@@ -31,10 +31,10 @@ static int ManhatanDistance((int x1, int y1) t1, (int x2, int y2) t2)
     return Math.Abs(t1.x1 - t2.x2) + Math.Abs(t1.y1 - t2.y2);
 }
 
-static int ParcelRequired(int tMins)
+static int ParcelRequired(double tMins)
 {
-    double x = tMins / 10;
-    if (x % 10 < 5)
+    double x = tMins / 10.0;
+    if (tMins % 10 < 5)
     {
         return (int)Math.Floor(x);
     }
@@ -47,8 +47,8 @@ static int ParcelRequired(int tMins)
 List<(int, int)> tupleList = ParseInputString(input);
 tupleList.ForEach(tuple => Console.WriteLine(tuple));
 
-Console.WriteLine(ParcelRequired(14));
 Console.WriteLine(ParcelRequired(11));
+Console.WriteLine(ParcelRequired(14));
 Console.WriteLine(ParcelRequired(15));
 Console.WriteLine(ParcelRequired(18));
 Console.WriteLine(ParcelRequired(21));
